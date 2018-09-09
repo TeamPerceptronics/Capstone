8407168f85d0d530f70fc7f67a45862e3eda4bfb
Sven Kratz <sven.kratz@harman.com>
2018-09-08 22:38:11 -0700

    waypoint updater at partial walkthrough state

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,8 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+import numpy as np
+from scipy.spatial import KDTree
 
 import math
 
@@ -27,26 +29,87 @@ LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this n
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
+        rospy.loginfo("Initializing Waypoint Updater")
+
+
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+        #rospy.subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        #rospy.subscriber('/obstacle_waypoint'. )
+
+
 
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
 
-        rospy.spin()
+        # 2d list to hold waypoints input to KD tree
+        self.waypoints_2d = None
+        self.waypoint_tree = None
+
+        # custom loop function
+        self.loop()
+
+    def loop(self):
+        rate = rospy.Rate(50)
+        rospy.loginfo("waypoint updater loop setup")
+
+        while not rospy.is_shutdown():
+            if self.pose and self.base_waypoints:
+                #get closest waypoint
+                closest_waypoint_tdx = self.get_closest_waypoint_idx()
+                self.publish_waypoints(closest_waypoint_tdx)
+            rate.sleep()
+        rospy.loginfo("Runloop done")
+
+    def get_closest_waypoint_idx(self):
+        #rospy.loginfo("get_closest_waypoint_idx")
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+
+        # find closest waypoint in KD tree
+        closest_idx = self.waypoint_tree.query([x,y], 1)[1]
+
+        # closest ahead or behind vehicle?
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx -1]
+
+        # dot product to get sign of the two vectors vs. vehicle position
+        cl_vect= np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x,y])
+
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+
+
+    def publish_waypoints(self, closest_idx):
+        rospy.loginfo("publishing waypoints")
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx : closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        #rospy.loginfo("new pose")
+        self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        rospy.loginfo("got waypoints...")
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
+        rospy.loginfo("built KD tree")
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
